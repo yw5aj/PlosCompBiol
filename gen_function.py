5b1ba458c93c5923f8e46167e67a85c7b28d507d
Yuxiang Wang <yw5aj@virginia.edu>
2016-02-11 19:14:18 -0500

    Clean up gen_function

--- gen_function.py
+++ gen_function.py
@@ -10,7 +10,6 @@ The input of the function is stress, and output is current.
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 from model_constants import LIF_RESOLUTION
 
@@ -112,71 +111,4 @@ def stress_to_current(fine_time, fine_stress, tau1, tau2, k1, mode):
 
 # %% Main function
 if __name__ == '__main__':
-    rough_time = np.genfromtxt('./Shawn model data/dcon_disp2_time.csv',
-                               delimiter=',')
-    rough_stress = np.genfromtxt('./Shawn model data/'
-                                 'dcon_disp2_stress.csv', delimiter=',')
-    fine_time, fine_stress = prepare_stress(rough_time, rough_stress)
-    # tau1, tau2, k1 values
-    tau1_s = 0.003
-    tau1_m = 0.008
-    tau1_l = 0.013
-    tau2_s = 0.05
-    tau2_m = 0.2
-    tau2_l = 0.35
-    k1_s = 0.1
-    k1_m = 0.5
-    k1_l = 0.9
-    tau1_ap1 = 0.5  # New approach 1
-    tau2_ap1 = 1  # New approach 1
-    k1_ap1 = 0.1  # New approach 1
-    tau2_ap2 = 1  # New approach 2
-    k1_ap2 = 0.1  # New approach 2
-    tau1_ap3 = 0.5  # New approach 3
-    tau2_ap3 = 1  # New approach 3
-    k1_ap3 = 0.5  # New approach 3
-    # %% Choose current with desired parameters
-    # Standard parameters
-#    gen_current = stress_to_current(fine_time, fine_stress, tau1_m, tau2_m, k1_m, 'gen')
-#    nr_current = stress_to_current(fine_time, fine_stress, tau1_m, tau2_m, k1_m,  'nr')
-#    mc_current = stress_to_current(fine_time, fine_stress, tau1_m, tau2_m, k1_m,  'mc')
-#    # Change tau1
-#    gen_current_tau1s = stress_to_current(fine_time, fine_stress, tau1_s, tau2_m, k1_m, 'gen')
-#    gen_current_tau1l = stress_to_current(fine_time, fine_stress, tau1_l, tau2_m, k1_m, 'gen')
-#    # Change tau2
-#    gen_current_tau2s = stress_to_current(fine_time, fine_stress, tau1_m, tau2_s, k1_m, 'gen')
-#    gen_current_tau2l = stress_to_current(fine_time, fine_stress, tau1_m, tau2_l, k1_m, 'gen')
-#    # Change k1
-#    gen_current_k1s = stress_to_current(fine_time, fine_stress, tau1_m, tau2_m, k1_s, 'gen')
-#    gen_current_k1l = stress_to_current(fine_time, fine_stress, tau1_m, tau2_m, k1_l, 'gen')
-#    # New approach 1: modify parameters
-#    gen_current = stress_to_current(fine_time, fine_stress, tau1_ap1, tau2_ap1, k1_ap1, 'nr')
-    # New approach 2: add tau 3 and c
-#    gen_current = stress_to_current(fine_time, fine_stress, tau1_m, tau2_ap2, k1_ap2, 'gen')
-    # New approach 2: add tau 3 and c
-    gen_current = stress_to_current(fine_time, fine_stress, tau1_ap3, tau2_ap3, k1_ap3, 'gen')
-    # %% Save files
-#    np.savetxt("disp2_fine_time.csv", fine_time, delimiter=",")
-#    np.savetxt("disp2_gen_current.csv", gen_current, delimiter=",")
-#    np.savetxt("disp2_nr_current.csv", nr_current, delimiter=",")
-#    np.savetxt("disp2_mc_current.csv", mc_current, delimiter=",")
-#    np.savetxt("disp2_tau1s_gen_current.csv", gen_current_tau1s, delimiter=",")
-#    np.savetxt("disp2_tau1l_gen_current.csv", gen_current_tau1l, delimiter=",")
-#    np.savetxt("disp2_tau2s_gen_current.csv", gen_current_tau2s, delimiter=",")
-#    np.savetxt("disp2_tau2l_gen_current.csv", gen_current_tau2l, delimiter=",")
-#    np.savetxt("disp2_k1s_gen_current.csv", gen_current_k1s, delimiter=",")
-#    np.savetxt("disp2_k1l_gen_current.csv", gen_current_k1l, delimiter=",")
-    # %% Plot
-    fig, axs = plt.subplots()
-    d2_cur_t = np.genfromtxt('./figure files/data/disp2_fine_time.csv',
-                             delimiter=',')
-    d2_gen_cur = np.genfromtxt('./figure files/data/disp2_gen_current.csv',
-                               delimiter=',')
-    axs.plot(d2_cur_t, -d2_gen_cur*1e9, color='0')
-    axs.plot(fine_time, -gen_current*1e9, color='0.5')
-    axs.set_xlim(0, 5)
-    axs.set_ylim(-15, 0)
-    axs.set_yticks([-15, -10, -5, 0])
-    axs.set_xlabel('Time (sec)')
-    axs.set_ylabel('Current (pA)')
-#    axs.plot(fine_time, -gen_current*1e9)  # in pA
+    pass
--- setup_cy_lif_model.py
+++ setup_cy_lif_model.py
@@ -7,11 +7,8 @@ import numpy as np
 
 extensions = [
     Extension('cy_lif_model',
-        ['cy_lif_model.pyx'],
-        include_dirs = [np.get_include()],
-        script_args = ['--compiler=mingw32'])
+              ['cy_lif_model.pyx'],
+              include_dirs=[np.get_include()])
     ]
 
-setup(
-    ext_modules = cythonize(extensions)
-    )
+setup(ext_modules=cythonize(extensions))
