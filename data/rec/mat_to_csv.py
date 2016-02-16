5495145ec5e22a66bb89c3de8e61f7f3a3a7d542
Yuxiang Wang <yw5aj@virginia.edu>
2016-02-16 14:47:19 -0500

    Added time window for recroding data

--- data/rec/mat_to_csv.py
+++ data/rec/mat_to_csv.py
@@ -12,7 +12,8 @@ import matplotlib.pyplot as plt
 from scipy.io import loadmat
 
 
-fs = int(1.6e3)
+fs = int(16e3)
+duration = 5
 animal_list = ['Piezo2CONT', 'Piezo2CKO', 'Atoh1CKO']
 mat_fname_dict = {
     'Piezo2CONT': '2013-12-07-01Piezo2CONT_calibrated.mat',
@@ -28,14 +29,22 @@ stim_num = len(next(iter(stim_list_dict.values())))
 def extract_trace_arr_dict(data_dict, stim_list, fs):
     trace_arr_dict = {'force': [], 'displ': [], 'spike': [], 'time': []}
     for block_id, stim_id in stim_list:
+        # Find the first spike index
+        spike_time = data_dict['OUT_PUT_CS%d' % block_id].T[stim_id]
+        start_index = spike_time.nonzero()[0][0]
+        end_index = start_index + duration * fs
+        data_range = range(start_index, end_index)
+        # Spike times
+        trace_arr_dict['spike'].append(spike_time[data_range])
+        # Force in mN
         trace_arr_dict['force'].append(
-            data_dict['COUT_PUT_F%d' % block_id].T[stim_id])
+            data_dict['COUT_PUT_F%d' % block_id].T[stim_id][data_range])
+        # Displ in mm
         trace_arr_dict['displ'].append(
-            data_dict['COUT_PUT_D%d' % block_id].T[stim_id] * 1e-3)  # to mm
-        trace_arr_dict['spike'].append(
-            data_dict['OUT_PUT_CS%d' % block_id].T[stim_id])
+            data_dict['COUT_PUT_D%d' % block_id].T[stim_id][data_range] * 1e-3)
+        # Time in ms
         trace_arr_dict['time'].append(
-            np.arange(trace_arr_dict['force'][-1].size) / fs * 1e3)  # to ms
+            np.arange(trace_arr_dict['force'][-1].size) / fs * 1e3)
     for key, item in trace_arr_dict.items():
         trace_arr_dict[key] = np.column_stack(item)
     return trace_arr_dict
@@ -73,9 +82,13 @@ def export_inst_fr_to_csv(trace_arr_dict_dict):
                                               trace_arr_dict['spike'].T[i])
             np.savetxt('./%s_fr_%d.csv' % (animal, i),
                        np.c_[spike_time, inst_fr], delimiter=',')
+            fig, axs = plt.subplots()
+            axs.plot(spike_time, inst_fr, '.k')
+            fig.savefig('./plots/%s_fr_%d.png' % (animal, i), dpi=300)
+            plt.close(fig)
 
 
-def plot_all(trace_arr_dict_dict):
+def plot_trace(trace_arr_dict_dict):
     for animal, trace_arr_dict in trace_arr_dict_dict.items():
         for key, trace_arr in trace_arr_dict.items():
             if key != 'time':
@@ -88,5 +101,5 @@ def plot_all(trace_arr_dict_dict):
 if __name__ == '__main__':
     trace_arr_dict_dict = export_trace_to_csv(
         animal_list, mat_fname_dict, stim_list_dict, fs)
-    plot_all(trace_arr_dict_dict)
+    plot_trace(trace_arr_dict_dict)
     export_inst_fr_to_csv(trace_arr_dict_dict)
--- fit_model.py
+++ fit_model.py
@@ -5,10 +5,36 @@ Created on Tue Feb 16 10:25:32 2016
 @author: Administrator
 """
 
-from lmfit import model
+import numpy as np
+import os
+from lmfit import Parameters, minimize, fit_report
+import matplotlib.pyplot as plt
 
 from stress_to_spike import stress_to_inst_fr
+from model_constants import MC_GROUPS
 
 
 def get_fine_fr():
     return time, fine_fr
+
+
+def load_rec_fr(animal, stim):
+    fname = os.path.join('data', 'rec', '%s_fr_%d.csv' % (animal, stim))
+    spike_time, inst_fr = np.genfromtxt(fname, delimiter=',').T
+    return exp_fr
+
+
+if __name__ == '__main__':
+    from setup_test_data import params, load_test_data
+    vname_list = ['fine_time', 'fine_stress']
+    data = load_test_data(vname_list)
+    spike_time, inst_fr = stress_to_inst_fr(
+        data['fine_time'], data['fine_stress'], MC_GROUPS, **params)
+    plt.plot(spike_time, inst_fr)
+    animal = 'Piezo2CONT'
+    stim = 0
+    fname = os.path.join('data', 'rec', '%s_fr_%d.csv' % (animal, stim))
+    spike_time, inst_fr = np.genfromtxt(fname, delimiter=',').T
+    plt.plot(spike_time, inst_fr, '.r')
+    plt.plot(data['fine_time'], data['fine_stress'] / 100, '-g')
+
