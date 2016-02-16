a3c1c7fc0a2cbec92c42b2c2de408cccddeaccea
Yuxiang Wang <yw5aj@virginia.edu>
2016-02-15 19:44:36 -0500

    Removed tests to compare with Kate

--- data/fem/unit_from_si.py
+++ data/fem/unit_from_si.py
@@ -15,11 +15,11 @@ if __name__ == '__main__':
 
     for fname in os.listdir(pname):
         if '_displ' in fname:
-            scale = 1e3
+            scale = 1e3  # to mm
         elif '_force' in fname:
-            scale = 1e3
+            scale = 1e3  # to mN
         elif '_time' in fname:
-            scale = 1e3
+            scale = 1e3  # to ms
         elif '_stress' in fname:
-            scale = 1e-3
+            scale = 1e-3  # to kPa
         scale_and_save(fname, scale)
--- data/rec/mat_to_csv.py
+++ data/rec/mat_to_csv.py
@@ -8,15 +8,16 @@ Created on Mon Feb 15 17:15:54 2016
 
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.io import loadmat
 
 
 fs = int(1.6e3)
 animal_list = ['Piezo2CONT', 'Piezo2CKO', 'Atoh1CKO']
 mat_fname_dict = {
-    'Piezo2CONT': '2013-12-07-01Piezo2CONT.mat',
-    'Piezo2CKO': '2013-12-13-02Piezo2CKO.mat',
-    'Atoh1CKO': '2013-10-16-01Atoh1CKO.mat'}
+    'Piezo2CONT': '2013-12-07-01Piezo2CONT_calibrated.mat',
+    'Piezo2CKO': '2013-12-13-02Piezo2CKO_calibrated.mat',
+    'Atoh1CKO': '2013-10-16-01Atoh1CKO_calibrated.mat'}
 stim_list_dict = {
     'Piezo2CONT': [(101, 2), (101, 1), (101, 3)],
     'Piezo2CKO': [(201, 2), (201, 7), (201, 4)],
@@ -27,13 +28,13 @@ def extract_trace_arr_dict(data_dict, stim_list, fs):
     trace_arr_dict = {'force': [], 'displ': [], 'spike': [], 'time': []}
     for block_id, stim_id in stim_list:
         trace_arr_dict['force'].append(
-            data_dict['OUT_PUT_F%d' % block_id].T[stim_id])
+            data_dict['COUT_PUT_F%d' % block_id].T[stim_id])
         trace_arr_dict['displ'].append(
-            data_dict['OUT_PUT_D%d' % block_id].T[stim_id])
+            data_dict['COUT_PUT_D%d' % block_id].T[stim_id] * 1e-3)  # to mm
         trace_arr_dict['spike'].append(
             data_dict['OUT_PUT_CS%d' % block_id].T[stim_id])
         trace_arr_dict['time'].append(
-            np.arange(trace_arr_dict['force'][-1].size) / fs)
+            np.arange(trace_arr_dict['force'][-1].size) / fs * 1e3)  # to ms
     for key, item in trace_arr_dict.items():
         trace_arr_dict[key] = np.column_stack(item)
     return trace_arr_dict
@@ -57,5 +58,17 @@ def export_all_to_csv(animal_list, mat_fname_dict, stim_list_dict, fs):
     return trace_arr_dict_dict
 
 
+def plot_all(trace_arr_dict_dict):
+    for animal, trace_arr_dict in trace_arr_dict_dict.items():
+        for key, trace_arr in trace_arr_dict.items():
+            if key != 'time':
+                fig, axs = plt.subplots()
+                axs.plot(trace_arr)
+                fig.savefig('./plots/%s_%s.png' % (animal, key), dpi=300)
+                plt.close(fig)
+
+
 if __name__ == '__main__':
-    export_all_to_csv(animal_list, mat_fname_dict, stim_list_dict, fs)
+    trace_arr_dict_dict = export_all_to_csv(
+        animal_list, mat_fname_dict, stim_list_dict, fs)
+    plot_all(trace_arr_dict_dict)
